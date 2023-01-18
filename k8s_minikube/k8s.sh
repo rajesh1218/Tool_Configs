0d9abf762d16faffd1595cc05d39f551ec59e96c
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2023-01-18 09:08:51 +0000

    Add files via upload

---
+++ k8s_minikube/k8s.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt install -y curl wget apt-transport-https
+
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+
+sudo chmod +x /usr/local/bin/minikube
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x kubectl
+
+sudo mv kubectl /usr/local/bin/
+
+kubectl version -o yaml
+
+minikube start --driver=docker
+
